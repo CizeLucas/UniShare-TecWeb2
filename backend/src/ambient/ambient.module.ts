6e825987ad691593fb7e2379a04d073d392b51ba
Cizé Lucas <cizelucas@gmail.com>
2026-06-29 19:06:57 -0300

    feat: removes old backend structure

--- backend/src/ambient/ambient.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AmbientService } from './ambient.service';
-import { AmbientController } from './ambient.controller';
-
-@Module({
-  controllers: [AmbientController],
-  providers: [AmbientService],
-})
-export class AmbientModule {}
